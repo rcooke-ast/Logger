040ee1b394f109ddbbf30bd74476facf63c2abc9
rcooke-ast <rcooke.ast@gmail.com>
2019-05-28 15:30:14 +0100

    training with redshift

--- FastFit/cnn_find_model.py
+++ FastFit/cnn_find_model.py
@@ -16,8 +16,8 @@ from keras.layers import Dropout, BatchNormalization
 
 vpix = 2.5   # Size of each pixel in km/s
 scalefact = np.log(1.0 + vpix/299792.458)
-spec_len = 65  # Number of pixels to use in each segment (must be odd)
-nHIwav = 15    # Number of lyman series lines to consider
+spec_len = 129  # Number of pixels to use in each segment (must be odd)
+nHIwav = 4    # Number of lyman series lines to consider
 atmdata = load_atomic(return_HIwav=False)
 ww = np.where(atmdata["Ion"] == "1H_I")
 HIwav = atmdata["Wavelength"][ww][3:]
@@ -32,22 +32,27 @@ def load_dataset(zem=3.0, snr=0, ftrain=0.75, numspec=20, epochs=10):
     IDlabel_all = np.load("train_data/cnn_qsospec_fluxspec_{0:s}_IDlabelonly.npy".format(extstr)).astype(np.int)
     Nlabel_all = np.load("train_data/cnn_qsospec_fluxspec_{0:s}_Nlabelonly.npy".format(extstr))
     blabel_all = np.load("train_data/cnn_qsospec_fluxspec_{0:s}_blabelonly.npy".format(extstr))
+    zlabel_all = np.load("train_data/cnn_qsospec_fluxspec_{0:s}_zlabelonly.npy".format(extstr))
     ntrain = int(ftrain*fdata_all.shape[0])
     speccut = epochs*((fdata_all.shape[1]-spec_len)//epochs)
-    trainX = fdata_all[:ntrain, -speccut:]
-    trainy = IDlabel_all[:ntrain, -speccut:]
-    trainN = Nlabel_all[:ntrain, -speccut:]
-    trainb = blabel_all[:ntrain, -speccut:]
-    testX = fdata_all[ntrain:, -speccut:]
-    testy = IDlabel_all[ntrain:, -speccut:]
-    testN = Nlabel_all[ntrain:, -speccut:]
-    testb = blabel_all[ntrain:, -speccut:]
+    trainX = fdata_all[:ntrain, -speccut:, 0]
+    trainy = IDlabel_all[:ntrain, -speccut:, 0]
+    trainN = Nlabel_all[:ntrain, -speccut:, 0]
+    trainz = zlabel_all[:ntrain, -speccut:, 0]
+    trainb = blabel_all[:ntrain, -speccut:, 0]
+    testX = fdata_all[ntrain:, -speccut:, 0]
+    testy = IDlabel_all[ntrain:, -speccut:, 0]
+    testN = Nlabel_all[ntrain:, -speccut:, 0]
+    testz = zlabel_all[ntrain:, -speccut:, 0]
+    testb = blabel_all[ntrain:, -speccut:, 0]
     print(trainX.shape[1], trainX.shape[1]//epochs, trainX.shape[1]%epochs)
-    return trainX, trainy, trainN, trainb, testX, testy, testN, testb
+    return trainX, trainy, trainN, trainz, trainb, testX, testy, testN, testz, testb
 
 
-def generate_data(data, IDlabels, Nlabels, blabels):
-    cntr_spec = data.shape[1]-spec_len
+def generate_data(data, IDlabels, Nlabels, zlabels, blabels):
+    shuff = np.arange(data.shape[1]-spec_len, dtype=np.int)
+    np.random.shuffle(shuff)
+    cntr_spec = 0
     IDarr = np.arange(IDlabels.shape[0], dtype=np.int)
     while True:
         indict = ({})
@@ -55,40 +60,42 @@ def generate_data(data, IDlabels, Nlabels, blabels):
             X_batch = np.ones((data.shape[0], spec_len, nHIwav))
             for nn in range(nHIwav):
                 nshft = int(np.round(np.log(HIwav[nn]/HIwav[ll])/scalefact))
-                lmin = cntr_spec+nshft
+                lmin = shuff[cntr_spec]+nshft
                 lmax = lmin + spec_len
                 if lmin < 0 or lmax >= data.shape[1]:
                     # Gone off the edge of the spectrum
                     continue
                 X_batch[:, :, nn] = data[:, lmin:lmax]
             indict['Ly{0:d}'.format(ll + 1)] = X_batch.copy()
+        midid = shuff[cntr_spec]+(spec_len-1)//2
         ID_batch = np.zeros((IDlabels.shape[0], 1 + nHIwav))
-        ID_batch[(IDarr, IDlabels[:, cntr_spec+(spec_len-1)//2],)] = 1
-        N_batch = Nlabels[:, cntr_spec+(spec_len-1)//2]
-        b_batch = blabels[:, cntr_spec+(spec_len-1)//2]
-        outdict = {'ID_output': ID_batch, 'N_output': N_batch, 'b_output': b_batch}
-        cntr_spec -= 1
+        ID_batch[(IDarr, IDlabels[:, midid],)] = 1
+        N_batch = Nlabels[:, midid]
+        z_batch = zlabels[:, midid]
+        b_batch = blabels[:, midid]
+        outdict = {'ID_output': ID_batch, 'N_output': N_batch, 'z_output': z_batch, 'b_output': b_batch}
+        cntr_spec += 1
         yield (indict, outdict)
 
         # restart counter to yield data in the next epoch as well
-        if cntr_spec <= 0:
-            cntr_spec = data.shape[1]-spec_len
+        if cntr_spec >= data.shape[1]-spec_len:
+            cntr_spec = 0
 
 
 # fit and evaluate a model
-def evaluate_model(trainX, trainy, trainN, trainb,
-                   testX, testy, testN, testb,
+def evaluate_model(trainX, trainy, trainN, trainz, trainb,
+                   testX, testy, testN, testz, testb,
                    epochs=10, verbose=1):
     inputs = []
     concat_arr = []
     for ll in range(nHIwav):
         inputs.append(Input(shape=(spec_len, nHIwav), name='Ly{0:d}'.format(ll+1)))
-        conv11 = Conv1D(filters=16, kernel_size=3, activation='relu')(inputs[-1])
-        pool11 = MaxPooling1D(pool_size=2)(conv11)
+        conv11 = Conv1D(filters=100, kernel_size=32, activation='relu')(inputs[-1])
+        pool11 = MaxPooling1D(pool_size=6)(conv11)
         norm11 = BatchNormalization()(pool11)
-        conv12 = Conv1D(filters=16, kernel_size=5, activation='relu')(norm11)
+        conv12 = Conv1D(filters=100, kernel_size=16, activation='relu')(norm11)
 #        drop11 = Dropout(rate=0.5)(conv12)
-        pool12 = MaxPooling1D(pool_size=2)(conv12)
+        pool12 = MaxPooling1D(pool_size=6)(conv12)
         norm12 = BatchNormalization()(pool12)
         concat_arr.append(Flatten()(norm12))
     # merge input models
@@ -98,8 +105,9 @@ def evaluate_model(trainX, trainy, trainN, trainb,
     fullcon = Dense(100, activation='relu')(merge)
     ID_output = Dense(1+nHIwav, activation='softmax', name='ID_output')(fullcon)
     N_output = Dense(1, activation='linear', name='N_output')(fullcon)
+    z_output = Dense(1, activation='linear', name='z_output')(fullcon)
     b_output = Dense(1, activation='linear', name='b_output')(fullcon)
-    model = Model(inputs=inputs, outputs=[ID_output, N_output, b_output])
+    model = Model(inputs=inputs, outputs=[ID_output, N_output, z_output, b_output])
     # Summarize layers
     print(model.summary())
     # Plot graph
@@ -107,22 +115,23 @@ def evaluate_model(trainX, trainy, trainN, trainb,
     # Compile
     loss = {'ID_output': 'categorical_crossentropy',
             'N_output': 'mse',
+            'z_output': 'mse',
             'b_output': 'mse'}
     model.compile(loss=loss, optimizer='adam', metrics=['accuracy'])
     # Fit network
     model.fit_generator(
-        generate_data(trainX, trainy, trainN, trainb),
+        generate_data(trainX, trainy, trainN, trainz, trainb),
         steps_per_epoch=(trainX.shape[1] - spec_len)//epochs,
         epochs=epochs, verbose=verbose,
-        validation_data=generate_data(testX, testy, testN, testb),
+        validation_data=generate_data(testX, testy, testN, testz, testb),
         validation_steps=(testX.shape[1] - spec_len)//epochs)
 
     # Eevaluate model
-    _, accuracy = model.evaluate_generator(generate_data(testX, testy, testN, testb),
-                                           steps=(testX.shape[1] - spec_len)//epochs,
-                                           verbose=0)
-    print(accuracy)
-    return accuracy
+#    _, accuracy
+    accuracy = model.evaluate_generator(generate_data(testX, testy, testN, testz, testb),
+                                        steps=(testX.shape[1] - spec_len)//epochs,
+                                        verbose=0)
+    return accuracy[4:], model.metrics_names[4:]
 
 # Gold standard in cross-validation
 # from sklearn.model_selection import StratifiedKFold
@@ -137,24 +146,30 @@ def evaluate_model(trainX, trainy, trainN, trainb,
 
 # summarize scores
 def summarize_results(scores):
-    print(scores)
-    m, s = mean(scores), std(scores)
-    print('Accuracy: %.3f%% (+/-%.3f)' % (m, s))
+    keys = scores.keys()
+    for ii in keys:
+        m, s = mean(scores[ii]), std(scores[ii])
+        print('Accuracy: %.3f%% (+/-%.3f)' % (m, s))
 
 
 # Detect features in a dataset
 def localise_features(repeats=3, epochs=10):
     # load data
-    trainX, trainy, trainN, trainb, testX, testy, testN, testb = load_dataset(epochs=epochs)
+    trainX, trainy, trainN, trainz, trainb,\
+    testX, testy, testN, testz, testb = load_dataset(epochs=epochs)
     # repeat experiment
-    scores = list()
+    allscores = dict({})
     for r in range(repeats):
-        score = evaluate_model(trainX, trainy, trainN, trainb, testX, testy, testN, testb, epochs=epochs)
-        score = score * 100.0
-        print('>#%d: %.3f' % (r + 1, score))
-        scores.append(score)
+        scores, names = evaluate_model(trainX, trainy, trainN, trainz, trainb,
+                                       testX, testy, testN, testz, testb, epochs=epochs)
+        if r == 0:
+            for name in names:
+                allscores[name] = []
+        for ii, name in enumerate(names):
+            allscores[name].append(scores[ii] * 100.0)
+            print('%s >#%d: %.3f' % (name, r + 1, allscores[name][-1]))
     # summarize results
-    summarize_results(scores)
+    summarize_results(allscores)
 
 
 # run the experiment
