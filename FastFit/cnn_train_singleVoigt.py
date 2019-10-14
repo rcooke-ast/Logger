7bf270329e124c501a3df9de0b53c29d0e9d3b27
Ryan <rcooke-ast@gmail.com>
2019-10-14 15:29:34 +0100

    single voigt training

--- FastFit/cnn_train_singleVoigt.py
+++ FastFit/cnn_train_singleVoigt.py
@@ -1,3 +1,4 @@
+import os
 import pdb
 import numpy as np
 from pyigm.fN.fnmodel import FNModel
@@ -5,7 +6,33 @@ from pyigm.fN.mockforest import monte_HIcomp
 from scipy.special import wofz
 from utilities import generate_wave, rebin_subpix
 from matplotlib import pyplot as plt
+
+import tensorflow as tf
+import keras.backend as K
+from keras.utils import plot_model, multi_gpu_model
+from keras.callbacks import ModelCheckpoint, CSVLogger
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
+from keras.layers.merge import concatenate
+from keras.layers import Dropout, BatchNormalization
+from contextlib import redirect_stdout
+
 velstep = 2.5    # Pixel size in km/s
+spec_len = 256
+spec_ext = 64
+
+# Define custom loss
+def mse_mask():
+    # Create a loss function that adds the MSE loss to the mean of all squared activations of a specific layer
+    def loss(y_true, y_pred):
+        mask = tf.cast(tf.not_equal(y_true, 0.0), tf.float32)
+        return K.mean(mask * K.square(y_pred - y_true), axis=-1)
+    # Return a function
+    return loss
 
 
 def voigt(wave, params):
@@ -26,7 +53,7 @@ def voigt(wave, params):
 
 def make_spectra(NHI, zabs, bval, nsubpix=10, szstr=None, numspec=1):
     if szstr is None:
-        szstr = 256+64
+        szstr = spec_len+spec_ext
     nspec = NHI.size
     vprofs = np.zeros((szstr, NHI.size))
     for ss in range(nspec):
@@ -69,100 +96,122 @@ def generate_dataset(zem=3.0, snr=0, seed=1234, ftrain=0.9, nsubpix=10, numspec=
 
 def load_dataset(zem=3.0, snr=0, ftrain=0.9, epochs=10):
     zstr = "zem{0:.2f}".format(zem)
-    fdata = np.load("svoigt_prof_zem3_nsubpix10_numspec1000.npy")
-    Nlabel = np.load("svoigt_Nval_zem3_nsubpix10_numspec1000.npy")
-    blabel = np.load("svoigt_bval_zem3_nsubpix10_numspec1000.npy")
-    zlabel = ((256+64)//2)*np.ones(Nlabel.size)
+    fdata = np.load("train_data/svoigt_prof_zem3_nsubpix10_numspec1000.npy").T
+    Nlabel = np.load("train_data/svoigt_Nval_zem3_nsubpix10_numspec1000.npy")
+    blabel = np.load("train_data/svoigt_bval_zem3_nsubpix10_numspec1000.npy")
+    zlabel = ((spec_len+spec_ext)//2)*np.ones(Nlabel.size)
     ntrain = int(ftrain*fdata.shape[0])
     # Select the training data
     trainX = fdata[:ntrain, :]
-    trainN = Nlabel[:ntrain, :]
-    trainz = zlabel[:ntrain, :]
-    trainb = blabel[:ntrain, :]
+    trainN = Nlabel[:ntrain]
+    trainb = blabel[:ntrain]
     # Select the test data
     testX = fdata[ntrain:, :]
-    testN = Nlabel[ntrain:, :]
-    testz = zlabel[ntrain:, :]
-    testb = blabel[ntrain:, :]
-    print(trainX.shape[1], trainX.shape[1]//epochs, trainX.shape[1]%epochs)
-    return trainX, trainN, trainz, trainb, testX, testN, testz, testb
+    testN = Nlabel[ntrain:]
+    testb = blabel[ntrain:]
+    return trainX, trainN, trainb, testX, testN, testb
+
+
+def yield_data(data, Nlabels, blabels):
+    cntr_batch, batch_sz = 0, 10000
+    cenpix = (spec_len+spec_ext)//2
+    ll = np.arange(batch_sz).repeat(spec_len)
+    while True:
+        indict = ({})
+        pertrb = np.random.randint(0, spec_ext, batch_sz)
+        pp = pertrb.reshape((batch_sz, 1)).repeat(spec_len, axis=1) + np.arange(spec_len)
+        X_batch = data[ll+cntr_batch, pp.flatten()].reshape((batch_sz, spec_len))
+        indict['kern_1'] = X_batch.copy().reshape((batch_sz, spec_len, 1))
+        z_batch = spec_len//2 - cenpix + pertrb.copy()
+        outdict = {'N_output': Nlabels[cntr_batch:cntr_batch+batch_sz],
+                   'z_output': z_batch,
+                   'b_output': blabels[cntr_batch:cntr_batch+batch_sz]}
+
+#        pdb.set_trace()
+        yield (indict, outdict)
+
+        cntr_batch += batch_sz
+        if cntr_batch >= data.shape[0]-batch_sz:
+            cntr_batch = 0
 
 
 # fit and evaluate a model
-def evaluate_model(trainX, trainy, trainN, trainz, trainb,
-                   testX, testy, testN, testz, testb,
+def evaluate_model(trainX, trainN, trainb,
+                   testX, testN, testb,
                    epochs=10, verbose=1):
-#    generate_data_test(testX, testy, testN, testz, testb)
-#    assert(False)
+    #yield_data(trainX, trainN, trainb)
+    #assert(False)
     filepath = os.path.dirname(os.path.abspath(__file__))
-    model_name = "/fit_data/model_nLy{0:d}_speclen{1:d}multi4".format(nHIwav, spec_len[0])
+    model_name = "/fit_data/svoigt_speclen{0:d}".format(spec_len)
     inputs = []
     concat_arr = []
-    kernsz = [15, 9, 5, 3]
-    for ll in range(len(spec_len)):
-        inputs.append(Input(shape=(spec_len[ll],1), name='Lya_{0:d}'.format(ll+1)))
-        conv11 = Conv1D(filters=64, kernel_size=kernsz[ll], activation='relu')(inputs[-1])
-#        conv12 = Conv1D(filters=64, kernel_size=16, activation='relu')(conv11)
-#        pool1  = MaxPooling1D(pool_size=2)(conv11)
-        conv21 = Conv1D(filters=128, kernel_size=kernsz[ll], activation='relu')(conv11)
-#        conv22 = Conv1D(filters=128, kernel_size=16, activation='relu')(conv21)
-#        pool2  = MaxPooling1D(pool_size=2)(conv21)
-        conv31 = Conv1D(filters=256, kernel_size=kernsz[ll], activation='relu')(conv21)
-#        conv32 = Conv1D(filters=256, kernel_size=3, activation='relu')(conv31)
-#        pool3  = MaxPooling1D(pool_size=2)(conv31)
-#        conv41 = Conv1D(filters=512, kernel_size=3, activation='relu')(pool3)
-#        conv42 = Conv1D(filters=512, kernel_size=3, activation='relu')(conv41)
-#        pool4  = MaxPooling1D(pool_size=2)(conv42)
-        concat_arr.append(Flatten()(conv31))
-        #concat_arr.append(pool3)
-    if nHIwav == 1 and len(spec_len)==1:
+    kernsz = [16]
+    # Construct network
+    for kk in range(len(kernsz)):
+        # Shape is (batches, steps, channels)
+        # For example, a 3-color 1D image of side 100 pixels, dealt in batches of 32 would have a shape=(32,100,3)
+        print(spec_len)
+        inputs.append(Input(shape=(spec_len, 1), name='kern_{0:d}'.format(kk+1)))
+        conv11 = Conv1D(filters=64, kernel_size=kernsz[kk], activation='relu')(inputs[-1])
+        conv12 = Conv1D(filters=64, kernel_size=kernsz[kk], activation='relu')(conv11)
+        pool1  = MaxPooling1D(pool_size=2)(conv12)
+        conv21 = Conv1D(filters=128, kernel_size=kernsz[kk], activation='relu')(pool1)
+        conv22 = Conv1D(filters=128, kernel_size=16, activation='relu')(conv21)
+        pool2  = MaxPooling1D(pool_size=2)(conv22)
+        conv31 = Conv1D(filters=256, kernel_size=kernsz[kk], activation='relu')(pool2)
+        conv32 = Conv1D(filters=256, kernel_size=3, activation='relu')(conv31)
+        pool3  = MaxPooling1D(pool_size=2)(conv32)
+        conv41 = Conv1D(filters=512, kernel_size=3, activation='relu')(pool3)
+        conv42 = Conv1D(filters=512, kernel_size=3, activation='relu')(conv41)
+        pool4  = MaxPooling1D(pool_size=2)(conv42)
+        concat_arr.append(Flatten()(pool4))
+    if len(kernsz) == 1:
         merge = concat_arr[0]
     else:
         # merge input models
         merge = concatenate(concat_arr)
-    # interpretation model
-    #hidden2 = Dense(100, activation='relu')(hidden1)
+    # Interpretation model
     fullcon1 = Dense(4096, activation='relu')(merge)
     fullcon2 = Dense(4096, activation='relu')(fullcon1)
-    ID_output = Dense(1+nHIwav, activation='softmax', name='ID_output')(fullcon2)
     N_output = Dense(1, activation='linear', name='N_output')(fullcon2)
     z_output = Dense(1, activation='linear', name='z_output')(fullcon2)
     b_output = Dense(1, activation='linear', name='b_output')(fullcon2)
-    model = Model(inputs=inputs, outputs=[ID_output, N_output, z_output, b_output])
+    model = Model(inputs=inputs, outputs=[N_output, z_output, b_output])
+    # Make this work on multiple GPUs
+    gpumodel = multi_gpu_model(model, gpus=4)
     # Summarize layers
     with open(filepath + model_name + '.summary', 'w') as f:
         with redirect_stdout(f):
-            model.summary()
+            gpumodel.summary()
     # Plot graph
     pngname = filepath + model_name + '.png'
-    plot_model(model, to_file=pngname)
+    plot_model(gpumodel, to_file=pngname)
     # Compile
-    loss = {'ID_output': 'categorical_crossentropy',
-            'N_output': mse_mask(),
+    loss = {'N_output': mse_mask(),
             'z_output': mse_mask(),
             'b_output': mse_mask()}
-    model.compile(loss=loss, optimizer='adam', metrics=['accuracy'])
+    gpumodel.compile(loss=loss, optimizer='adam', metrics=['accuracy'])
     # Initialise callbacks
     ckp_name = filepath + model_name + '.hdf5'
     csv_name = filepath + model_name + '.log'
     checkpointer = ModelCheckpoint(filepath=ckp_name, verbose=1, save_best_only=True)
     csv_logger = CSVLogger(csv_name, append=True)
     # Fit network
-    model.fit_generator(
-        generate_data_multilen(trainX, trainy, trainN, trainz, trainb),
-        steps_per_epoch=(trainX.shape[1] - spec_len[0])//epochs,
+    gpumodel.fit_generator(
+        yield_data(trainX, trainN, trainb),
+        steps_per_epoch=2000,  # Total number of batches (i.e. num data/batch size)
         epochs=epochs, verbose=verbose,
         callbacks=[checkpointer, csv_logger],
-        validation_data=generate_data(testX, testy, testN, testz, testb),
-        validation_steps=(testX.shape[1] - spec_len[0])//epochs)
+        validation_data=yield_data(testX, testN, testb),
+        validation_steps=200)
 
     # Evaluate model
 #    _, accuracy
-    accuracy = model.evaluate_generator(generate_data_multilen(testX, testy, testN, testz, testb),
-                                        steps=(testX.shape[1] - spec_len[0])//epochs,
+    accuracy = gpumodel.evaluate_generator(yield_data(testX, testN, testb),
+                                        steps=testX.shape[0],
                                         verbose=0)
     #pdb.set_trace()
-    return accuracy, model.metrics_names
+    return accuracy, gpumodel.metrics_names
 
 # Gold standard in cross-validation
 # from sklearn.model_selection import StratifiedKFold
@@ -179,20 +228,20 @@ def evaluate_model(trainX, trainy, trainN, trainz, trainb,
 def summarize_results(scores):
     keys = scores.keys()
     for ii in keys:
-        m, s = mean(scores[ii]), std(scores[ii])
+        m, s = np.mean(scores[ii]), np.std(scores[ii])
         print('Accuracy: %.3f%% (+/-%.3f)' % (m, s))
 
 
 # Detect features in a dataset
 def localise_features(repeats=3, epochs=10):
     # load data
-    trainX, trainy, trainN, trainz, trainb,\
-    testX, testy, testN, testz, testb = load_dataset(epochs=epochs)
+    trainX, trainN, trainb,\
+    testX, testN, testb = load_dataset(epochs=epochs)
     # repeat experiment
     allscores = dict({})
     for r in range(repeats):
-        scores, names = evaluate_model(trainX, trainy, trainN, trainz, trainb,
-                                       testX, testy, testN, testz, testb, epochs=epochs)
+        scores, names = evaluate_model(trainX, trainN, trainb,
+                                       testX, testN, testb, epochs=epochs)
         if r == 0:
             for name in names:
                 allscores[name] = []
@@ -208,9 +257,9 @@ def localise_features(repeats=3, epochs=10):
 # Set the number of epochs
 epochs = 10
 
-# Generate data
-generate_dataset(epochs=epochs)
-
-# Once the data exist, run the experiment
 if False:
+    # Generate data
+    generate_dataset(epochs=epochs)
+else:
+    # Once the data exist, run the experiment
     localise_features(epochs=10)
